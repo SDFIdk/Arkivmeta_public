8373c930246110de646cf26fd9fb805b49b6c0b0
Nobralyra <48592380+Nobralyra@users.noreply.github.com>
2022-09-05 14:00:37 +0200

    Move information to slatedoc

--- src/main/java/dk/dataforsyningen/arkivmeta/configuration/SwaggerDocumentationConfig.java
+++ src/main/java/dk/dataforsyningen/arkivmeta/configuration/SwaggerDocumentationConfig.java
@@ -26,21 +26,10 @@ public class SwaggerDocumentationConfig
     @Bean
     public OpenAPI customImplementation()
     {
-        String apiInfoString = "";
-
-        apiInfoString = getAPIinfoDescription();
-
         return new OpenAPI()
                 .info(new Info()
                               .title("Arkivmeta")
-                              .version("1.0.11")
-                              .description(apiInfoString)
-                              .termsOfService("https://dataforsyningen.dk")
-                              .contact(new Contact().name("SDFE Support").url(
-                                      "https://dataforsyningen.dk").email(
-                                      "support@sdfe.dk"))
-                              .license(new License().name("Licensbetingelser")
-                                               .url("https://dataforsyningen.dk/Vilkaar")))
+                              .version("1.0.11"))
                 // Components section defines Security Scheme "mySecretHeader"
                 .components(new Components()
                                 .addSecuritySchemes("ApiKeyAuth", new SecurityScheme()
@@ -49,19 +38,5 @@ public class SwaggerDocumentationConfig
                                         .name("token")))
                 // AddSecurityItem section applies created scheme globally
                 .addSecurityItem(new SecurityRequirement().addList("ApiKeyAuth"));
-    }
-
-    private String getAPIinfoDescription()
-    {
-        try
-        {
-            File file = ResourceUtils.getFile("src/main/resources/APIinfo.md");
-            String apiInfoDescription = new String(Files.readAllBytes(file.toPath()));
-            return apiInfoDescription;
-        }
-        catch (IOException ioException)
-        {
-            throw new RuntimeException(ioException);
-        }
     }
 }
