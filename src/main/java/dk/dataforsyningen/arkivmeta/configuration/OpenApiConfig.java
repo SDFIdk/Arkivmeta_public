c57aac28c7f75e583605b4785d36ad642d090f32
Nobralyra <48592380+Nobralyra@users.noreply.github.com>
2022-09-13 13:28:00 +0200

    Add Authentication for token in header

--- src/main/java/dk/dataforsyningen/arkivmeta/configuration/OpenApiConfig.java
+++ src/main/java/dk/dataforsyningen/arkivmeta/configuration/OpenApiConfig.java
@@ -9,34 +9,39 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OpenApiConfig
-{
-    final String securitySchemeName = "token";
-    /**
-     *
-     * @return OpenAPI custom object
-     */
-    @Bean
-    public OpenAPI customImplementation()
-    {
-        return new OpenAPI()
-                .info(new Info()
-                    .title("Arkivmeta")
-                    .version("1.0.11")
-                    .description("""
-                        APIet __Arkivmeta__ giver adgang til at søge i metadata for en større samling historiske kort og benytte resultatet til at fremvise det skannede materiale.\s
-                                                
-                        Til adgang benyttes Kortforsyningens brugeradgang som ved andre tjenester.
-                                                
-                        Stier til kortfiler følger [IIIF specifikationen](https://iiif.io/) og kan vises med en viser, der understøtter dette.
-                        """))
-                // AddSecurityItem section applies created scheme globally
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-                // Components section defines Security Scheme
-                .components(new Components()
-                    .addSecuritySchemes(securitySchemeName, new SecurityScheme()
-                        .type(SecurityScheme.Type.APIKEY)
-                        .in(SecurityScheme.In.QUERY)
-                        .name("token")));
-    }
+public class OpenApiConfig {
+  final String securitySchemeNameQuery = "QueryToken";
+  final String securitySchemeNameHeader = "HeaderToken";
+
+
+  /**
+   * @return OpenAPI custom object
+   */
+  @Bean
+  public OpenAPI customImplementation() {
+    return new OpenAPI()
+        .info(new Info()
+            .title("Arkivmeta")
+            .version("1.0.11")
+            .description("""
+                APIet __Arkivmeta__ giver adgang til at søge i metadata for en større samling historiske kort og benytte resultatet til at fremvise det skannede materiale.\s
+                                        
+                Til adgang benyttes Kortforsyningens brugeradgang som ved andre tjenester.
+                                        
+                Stier til kortfiler følger [IIIF specifikationen](https://iiif.io/) og kan vises med en viser, der understøtter dette.
+                """))
+        // AddSecurityItem section applies created scheme/paths globally
+        .addSecurityItem(new SecurityRequirement().addList(securitySchemeNameHeader))
+        .addSecurityItem(new SecurityRequirement().addList(securitySchemeNameQuery))
+        // Components section defines Security Scheme
+        .components(new Components()
+            .addSecuritySchemes(securitySchemeNameHeader, new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("token"))
+            .addSecuritySchemes(securitySchemeNameQuery, new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.QUERY)
+                .name("token")));
+  }
 }
