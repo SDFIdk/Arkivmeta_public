c440184a4cf931b0aad440d28b91ec872e67aa7c
Nobralyra <48592380+Nobralyra@users.noreply.github.com>
2022-11-28 11:14:26 +0100

    Update to match other projects Errorhandling

--- src/main/java/dk/dataforsyningen/arkivmeta/errorhandling/ErrorResponse.java
+++ src/main/java/dk/dataforsyningen/arkivmeta/errorhandling/ErrorResponse.java
@@ -1,10 +1,47 @@
 package dk.dataforsyningen.arkivmeta.errorhandling;
 
+import java.util.List;
+import org.springframework.http.HttpStatus;
+
 public class ErrorResponse {
+  /** The HTTP status code */
+  private HttpStatus status;
+
+  /** The error message associated with exception */
   private String message;
+
+  /** List of constructed error messages */
+  private List<String> errors;
 
   public ErrorResponse(String message) {
     this.message = message;
+  }
+
+  public ErrorResponse(HttpStatus status, String message) {
+    this.status = status;
+    this.message = message;
+  }
+
+  public ErrorResponse(HttpStatus status, String message, List<String> errors) {
+    super();
+    this.status = status;
+    this.message = message;
+    this.errors = errors;
+  }
+
+  public ErrorResponse(HttpStatus status, String message, String error) {
+    super();
+    this.status = status;
+    this.message = message;
+    errors = List.of(error);
+  }
+
+  public HttpStatus getStatus() {
+    return status;
+  }
+
+  public void setStatus(HttpStatus status) {
+    this.status = status;
   }
 
   public String getMessage() {
@@ -13,5 +50,13 @@ public class ErrorResponse {
 
   public void setMessage(String message) {
     this.message = message;
+  }
+
+  public List<String> getErrors() {
+    return errors;
+  }
+
+  public void setErrors(List<String> errors) {
+    this.errors = errors;
   }
 }
