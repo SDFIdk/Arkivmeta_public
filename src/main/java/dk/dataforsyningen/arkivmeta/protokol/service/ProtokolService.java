7099aeb74a93d079950e93d2876a1d2bdc023ca8
Nobralyra <48592380+Nobralyra@users.noreply.github.com>
2023-02-27 09:58:30 +0100

    Add @service annotation

--- src/main/java/dk/dataforsyningen/arkivmeta/protokol/service/ProtokolService.java
+++ src/main/java/dk/dataforsyningen/arkivmeta/protokol/service/ProtokolService.java
@@ -4,17 +4,20 @@ import dk.dataforsyningen.arkivmeta.protokol.apimodel.ProtokolDto;
 import dk.dataforsyningen.arkivmeta.protokol.dao.IProtokolDao;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProtokolService implements IProtokolService {
   private final IProtokolDao iProtokolDao;
 
-  public ProtokolService(IProtokolDao iProtokolDao) {
+  public ProtokolService(@Qualifier("protokolDao") IProtokolDao iProtokolDao) {
     this.iProtokolDao = iProtokolDao;
   }
 
   @Override
   public ProtokolDto getProtokolById(String arketype, String id) {
-    String searchId = arketype + id;
+    String searchId = arketype + "/" + id;
 
     Optional<ProtokolDto> returnedProtokol = iProtokolDao.getProtokolById(searchId);
 
