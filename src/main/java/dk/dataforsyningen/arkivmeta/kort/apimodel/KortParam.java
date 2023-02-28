8285f69166c9122019b85e0525ff0540b0227f12
Nobralyra <48592380+Nobralyra@users.noreply.github.com>
2023-02-28 15:14:15 +0100

    Reuse the same KortParam to getKort as in postKort. Add checks that add default values to direction, limit, and offset if the client did not specify them

--- src/main/java/dk/dataforsyningen/arkivmeta/kort/apimodel/KortParam.java
+++ src/main/java/dk/dataforsyningen/arkivmeta/kort/apimodel/KortParam.java
@@ -1,51 +1,67 @@
 package dk.dataforsyningen.arkivmeta.kort.apimodel;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Pattern;
 import java.util.List;
 import java.util.Objects;
+import org.springframework.beans.factory.annotation.Value;
 
 public class KortParam {
+
+  @Parameter(description =
+      "Arketype, se /metadata/arketyper. Hvis der ønskes at søge på flere arketyper på en gang, skal man adskille hvert søgekriterie ved at bruge komma `,`. " +
+          "Eksempel: `arketype=matrikelkort,centimeterkort.`")
   @Schema(description = "Kortets arketype.")
   private List<String> arketype;
 
+  @Parameter(description =
+      "Dækningsområde, se /metadata/daekningsomraader. Hvis der ønskes at søge på flere dækningsområde på en gang, skal man adskille hvert søgekriterie ved at bruge `,`. " +
+          "Eksempel: `daekningsomraade=Slesvig,Danmark`")
   @Schema(description = "Geografisk område, som kortet dækker helt eller delvist. For eksempel Danmark, Grønland. Et kort kan have flere dækningsområder.For prøvekort og lignende vil dækningsområdet kunne angives som intet.")
   private List<String> daekningsomraade;
 
-  @Schema(description = "Sorteringsretning, 'asc' for stigende, 'desc' for faldende")
+  @Schema(description = "Sorteringsretning, `asc` for stigende, `desc` for faldende", defaultValue = "asc")
   @Pattern(regexp = "asc|desc")
   private String direction;
 
+  @Parameter(description = "Fritekstsøgning")
   @Schema(description = "Fritekstsøgning")
   private String fritekstsoegning;
 
-  @Schema(description = "Starttid for kortets gyldighedsperiode. Angives i hele år. Gyldighedsperiodens starttid er et korts trykke-, tegne-, optage- eller opmålingsår – dvs. det år hvor kortet kan siges at være nyeste kort.")
+  @Schema(description = "Starttid for kortets gyldighedsperiode. Angives i hele år, eksempel `1966`. Gyldighedsperiodens starttid er et korts trykke-, tegne-, optage- eller opmålingsår – dvs. det år hvor kortet kan siges at være nyeste kort.")
   private Integer gaeldendefra;
 
-  @Schema(description = "Sluttid for kortets gyldighedsperiode. Angives i hele år. Typisk fordi kortet erstattes af et nyere. Hvis der ikke er fundet en specifik gældende til periode angives et årstal der ligger 50 år efter gældende fra.")
+  @Schema(description = "Sluttid for kortets gyldighedsperiode. Angives i hele år, eksempel `1966`. Typisk fordi kortet erstattes af et nyere. Hvis der ikke er fundet en specifik gældende til periode angives et årstal der ligger 50 år efter gældende fra.")
   private Integer gaeldendetil;
 
-  @Schema(description = "Det geografiske område, ofte en polygon, som kortet ligger indenfor. WKT med SRS = EPSG:4326")
+  @Schema(description = "Geometri angives som WKT med SRS = EPSG:4326. Det geografiske område, ofte en polygon, som kortet ligger indenfor.")
   private String geometri;
 
-  @Schema(description = "Angivelse af geografisk område, f.eks. indenfor kortbladsinddelingen eller ejerlav. For topografiske kort er inddelingen et overordnet grid hvorimod matrikelkort er indelt efter ejerlav. Kan også være f.eks. administrative inddelinger.")
+  @Schema(description = "Angivelse af geografisk område, f.eks. indenfor kortbladsinddelingen eller ejerlav. For topografiske kort er inddelingen et overordnet grid hvorimod matrikelkort er inddelt efter ejerlav. Kan også være f.eks. administrative inddelinger.")
   private String kortbladnummer;
 
+  @Parameter(description =
+      "Kortværk. Hvis der ønskes at søge på flere kortværker på en gang, skal man angive `kortvaerk` query parameteren for hvert eneste en kortværk man vil søge efter." +
+          "Eksempel: `kortvaerk=Trap, tegnede kort`&`kortvaerk=Mejer`")
   @Schema(description = "Navnet på en logisk samling af skannede kort som kortet hører til, f.eks. atlasblade.")
   private List<String> kortvaerk;
 
+  @Parameter(description =
+      "Målestoksforhold. Hvis der ønskes at søge på flere målestoksforhold på en gang, skal man adskille hvert søgekriterie ved at bruge `,`. " +
+          "Eksempel: `maalestok=1:40000,1:180000`")
   @Schema(description = "Størrelsesforholdet mellem landskabet og kortets repræsentation heraf.")
   private List<String> maalestok;
 
-  @Schema(description = "Sidestørrelse, dvs. hvor mange poster pr. side")
+  @Schema(description = "Sidestørrelse, dvs. hvor mange poster pr. side", defaultValue = "100")
   @Min(1)
   @Max(1000)
-  private int limit;
+  private Integer limit;
 
-  @Schema(description = "Offset, dvs. fra hvilken post")
-  private int offset;
+  @Schema(description = "Offset, dvs. fra hvilken post", defaultValue = "0")
+  private Integer offset;
 
   @Schema(description = "Sorteringsfelt, kan sortere på følgende typer: arketype, daekningsomraade, gaeldendefra, gaeldendetil, id, kortvaerk, maalestok, titel")
   private String sort;
@@ -59,7 +75,7 @@ public class KortParam {
   public KortParam(List<String> arketype, List<String> daekningsomraade, String direction,
                    String fritekstsoegning, Integer gaeldendefra, Integer gaeldendetil,
                    String geometri, String kortbladnummer, List<String> kortvaerk,
-                   List<String> maalestok, int limit, int offset, String sort, String tegner,
+                   List<String> maalestok, Integer limit, Integer offset, String sort, String tegner,
                    String titel) {
     this.arketype = arketype;
     this.daekningsomraade = daekningsomraade;
@@ -158,19 +174,19 @@ public class KortParam {
     this.maalestok = maalestok;
   }
 
-  public int getLimit() {
+  public Integer getLimit() {
     return limit;
   }
 
-  public void setLimit(int limit) {
+  public void setLimit(Integer limit) {
     this.limit = limit;
   }
 
-  public int getOffset() {
+  public Integer getOffset() {
     return offset;
   }
 
-  public void setOffset(int offset) {
+  public void setOffset(Integer offset) {
     this.offset = offset;
   }
 
@@ -196,38 +212,5 @@ public class KortParam {
 
   public void setTitel(String titel) {
     this.titel = titel;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    KortParam kortParam = (KortParam) o;
-    return gaeldendefra == kortParam.gaeldendefra
-        && gaeldendetil == kortParam.gaeldendetil
-        && limit == kortParam.limit
-        && offset == kortParam.offset
-        && Objects.equals(arketype, kortParam.arketype)
-        && Objects.equals(daekningsomraade, kortParam.daekningsomraade)
-        && Objects.equals(direction, kortParam.direction)
-        && Objects.equals(fritekstsoegning, kortParam.fritekstsoegning)
-        && Objects.equals(geometri, kortParam.geometri)
-        && Objects.equals(kortbladnummer, kortParam.kortbladnummer)
-        && Objects.equals(kortvaerk, kortParam.kortvaerk)
-        && Objects.equals(maalestok, kortParam.maalestok)
-        && Objects.equals(sort, kortParam.sort)
-        && Objects.equals(tegner, kortParam.tegner)
-        && Objects.equals(titel, kortParam.titel);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(arketype, daekningsomraade, direction, fritekstsoegning, gaeldendefra,
-        gaeldendetil,
-        geometri, kortbladnummer, kortvaerk, maalestok, limit, offset, sort, tegner, titel);
   }
 }
