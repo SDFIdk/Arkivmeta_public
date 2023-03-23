d88a6b7148710f126a31c3abf9c0a1c39585075b
Nobralyra <48592380+Nobralyra@users.noreply.github.com>
2023-03-23 12:31:38 +0100

    Clean up

--- src/main/java/dk/dataforsyningen/arkivmeta/kort/apimodel/KortParam.java
+++ src/main/java/dk/dataforsyningen/arkivmeta/kort/apimodel/KortParam.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import java.util.List;
 import java.util.Objects;
