4da08d47db7f0892eef3a03594a49c7a6bb3a9fd
seskildsen <sines@sdfi.dk>
2025-01-23 12:49:04 +0100

    feat: update openapi version to match new v3.0.0 version

--- src/main/java/dk/dataforsyningen/arkivmeta/configuration/OpenApiConfig.java
+++ src/main/java/dk/dataforsyningen/arkivmeta/configuration/OpenApiConfig.java
@@ -22,7 +22,7 @@ public class OpenApiConfig {
     return new OpenAPI()
         .info(new Info()
             .title("Arkivmeta")
-            .version("2.0.0")
+            .version("3.0.0")
             .description("""
                 APIet __Arkivmeta__ giver adgang til at søge i metadata for en større samling historiske kort, dokumenter og benytte resultatet til at fremvise det skannede materiale.
                 
